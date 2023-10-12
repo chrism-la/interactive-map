07d778d8d7825584ed8fa3af4c2c2a8fd49a8211
Chris <117781333+chrism-la@users.noreply.github.com>
2023-10-11 19:45:13 -0700

    edits

--- map.js
+++ map.js
@@ -1,9 +1,10 @@
+// create Map
 const myMap = L.map('map', {
     center: [34.0206085, -118.741372],
     zoom: 8,
 });
 
-// Create the Tiles
+// Create Tile Layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(myMap);
@@ -12,6 +13,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 const marker = L.marker([34.0206085, -118.741372]);
 marker.addTo(myMap).bindPopup('<p1><b>You Are Here</b></p1>').openPopup();
 
+// function to retrieve user location
 async function getUserCoordinates() {
     let position = await new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -23,3 +25,11 @@ async function getUserCoordinates() {
 // then initialize map
 // listen for events on the menu
 // then apply the foursquare
+
+// map object
+// get coords via geolocation
+// get foursquare busin
+// process foursquare array
+// event handlers
+// window load
+// business submit
